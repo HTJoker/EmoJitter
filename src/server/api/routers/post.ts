4d4fe54c88e09079a6391da9837cc65f2e8b661c
Hersh Thakur <hersh.thakur@gmail.com>
2023-11-16 14:40:48 +0000

    get post for profile page

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,6 +2,7 @@ import { clerkClient } from "@clerk/nextjs";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { filterUserForClient } from "~/utils/helpers";
+import type { Post } from "@prisma/client";
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
@@ -12,13 +13,38 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
+const addUserDataToPost = async (post: Post[]) => {
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: post.map((p) => p.authorId),
+      limit: 100,
+    })
+  ).map(filterUserForClient);
+
+  return post.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+
+    if (!author)
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author not found",
+      });
+
+    return {
+      post,
+      author: {
+        ...author,
+        username: author.username,
+      },
+    };
+  });
+};
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(3, "1 m"),
   analytics: true,
 });
-
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -28,29 +54,20 @@ export const postRouter = createTRPCRouter({
         createdAt: "desc",
       },
     });
-
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: post.map((p) => p.authorId),
-        limit: 100,
-      })
-    ).map(filterUserForClient);
-
-    return post.map((post) => {
-      const author = users.find((user) => user.id === post.authorId);
-
-      if (!author)
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author not found",
-        });
-
-      return {
-        post,
-        author,
-      };
-    });
+    return await addUserDataToPost(post);
   }),
+
+  getPostsByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ ctx, input }) =>
+      ctx.db.post.findMany({
+        where: {
+          authorId: input.userId,
+        },
+        take: 5,
+        orderBy: [{ createdAt: "desc" }],
+      }).then(addUserDataToPost),
+    ),
 
   create: privateProcedure
     .input(
